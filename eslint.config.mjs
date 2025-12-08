1993ccecc1cdd24016d31c598e24bc3a0ea016cf
Gurgen Torosyan <gurgen.t@solargenix.ai>
2025-12-08 12:15:49 +0400

    feat(DEV-2255): fix Security vulnerability issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,18 +6,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
-  // Base JS rules
   js.configs.recommended,
-
-  // TypeScript rules
   ...tseslint.configs.recommended,
-
-  // Next.js recommended rules (flat config)
   nextPlugin.configs.recommended,
-
-  // React Flat Config
   reactPlugin.configs.flat.recommended,
-
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
 
@@ -40,30 +32,19 @@ export default [
     },
 
     rules: {
-      // Prettier
       'prettier/prettier': 'error',
-
-      // SVG exceptions
       'react/no-unknown-property': [
         'error',
         {
           ignore: ['stroke-width', 'fill-rule', 'clip-rule', 'stroke-linecap', 'stroke-linejoin'],
         },
       ],
-
-      // Hooks
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
-
-      // TypeScript
       '@typescript-eslint/no-unused-vars': 'warn',
-
-      // React (Next.js does not require React import)
       'react/react-in-jsx-scope': 'off',
     },
   },
-
-  // Ignored files
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
