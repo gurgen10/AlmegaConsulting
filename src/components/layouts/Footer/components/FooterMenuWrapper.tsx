6f842c04de3587013750c576118d3eb9fcc06a0b
Gurgen Torosyan <gurgen.t@solargenix.ai>
2025-12-09 16:09:28 +0400

    feat(layout): change base layout

--- src/components/layouts/Footer/components/FooterMenuWrapper.tsx
+++ src/components/layouts/Footer/components/FooterMenuWrapper.tsx
@@ -1,7 +1,8 @@
 import { Typography } from '@mui/material';
 import { FooterLinkHeader } from './Footer.style';
+import { ReactNode } from 'react';
 
-const FooterMenuWrapper = ({ children }: { children: React.ReactNode | string }) => {
+const FooterMenuWrapper = ({ children }: { children: ReactNode | string }) => {
   return (
     <FooterLinkHeader>
       <Typography color="tertiary.300" variant="body1">
