c5e2551c1188b5df1cf71f5c82f2d52e81ff0d6e
Gurgen Torosyan <gurgen.t@solargenix.ai>
2026-01-08 18:30:05 +0400

    fix(features): fix line gradient

--- src/components/ui/GradientDivider/index.tsx
+++ src/components/ui/GradientDivider/index.tsx
@@ -3,8 +3,10 @@ import { Box } from '@mui/material';
 export default function GradientDivider() {
   return (
     <Box
-      height="1px"
-      sx={{ background: 'linear-gradient(90deg, #FAFAFA 0%, #96B0B0 50.15%, #FAFAFA 100%)' }}
+      sx={{
+        borderTop: '1px solid',
+        borderImage: 'linear-gradient(90deg, #FAFAFA 0%, #96B0B0 50.15%, #FAFAFA 100%) 1',
+      }}
     />
   );
 }
