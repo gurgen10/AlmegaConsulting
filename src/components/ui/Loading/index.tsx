da8dd12262342fd28f391e0d13264364d5b0e83b
Gurgen Torosyan <gurgen.t@solargenix.ai>
2026-01-14 19:12:41 +0400

    feature(loading): add Loading texts prop

--- src/components/ui/Loading/index.tsx
+++ src/components/ui/Loading/index.tsx
@@ -9,31 +9,7 @@ import LoadingIcon from './LoadingIcon';
 import { useTheme } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
 
-const loadingTexts = [
-  'Powering Tomorrow',
-  'Charging the Future',
-  'Go Solar Now',
-  'Brighten Your World',
-  'Shine Smarter',
-  'Live Sun-Driven',
-  'Switch to Clean',
-  'Unleash Clean Energy',
-  'Energize Green',
-  'Save More with the Sun',
-  'Solar That Pays You Back',
-  'The Smartest Energy Choice',
-  'The Future of Energy',
-  'Invest in the Sun',
-  'Skyrocket Your Solar Sales',
-  'Unlock Solar Sales Success',
-  'Turn Leads Into Solar Deals',
-  'Sun-Powered, Future-Ready',
-  'Smarter Homes Run on Solar',
-  'Start Saving from Day One',
-  'Stop Renting Power. Own It.',
-];
-
-export default function Loading() {
+export default function Loading({ loadingTexts = [] }: { loadingTexts: string[] }) {
   const [step, setStep] = useState(0);
   const stepRef = useRef(0);
   const theme = useTheme();
@@ -70,7 +46,7 @@ export default function Loading() {
         }, interval);
       }
     }
-  }, [step]);
+  }, [step, loadingTexts]);
 
   return (
     <Box
