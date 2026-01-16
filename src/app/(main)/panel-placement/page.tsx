edf21796dba0978d9d072cb1b9dd43268fc80ca4
Gurgen Torosyan <gurgen.t@solargenix.ai>
2026-01-16 15:52:15 +0400

    feat(meta): meta info for the new website product and features pages

--- src/app/(main)/panel-placement/page.tsx
+++ src/app/(main)/panel-placement/page.tsx
@@ -11,19 +11,10 @@ import PanelPlacementFaq from '@/components/common/panelPlacement/PanelPlacement
 import BottomCallToAction from '@/components/common/panelPlacement/BottomCallToAction';
 
 export const metadata: Metadata = {
-  title: 'Fastest AI Solar Sales Proposal Software | SolarGenix',
+  title: 'AI-powered Solar Panel Placement for Smart Layout | SolarGenix',
   description:
-    'Boost your solar sales with Solargenix.ai – the fastest and easiest AI-powered solar proposal creation tool. Automate and simplify your solar business with precision and speed.',
-  keywords: [
-    'solar sales proposal',
-    'AI solar proposal',
-    'fastest solar proposal tool',
-    'easy solar sales',
-    'Solargenix.ai',
-    'solar business automation',
-    'solar software',
-    'solar quoting tool',
-  ],
+    'Optimize panel layouts automatically with an AI-powered solar panel placement tool for faster, smarter designs.',
+  keywords: [],
 };
 export default async function Home() {
   return (
